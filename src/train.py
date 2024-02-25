e26ac40509542500754c9b7b5464ba7c5bef3ce1
ineslrr <92888859+ineslrr@users.noreply.github.com>
2024-02-25 18:04:58 +0100

    class for different agent types

--- src/train.py
+++ src/train.py
@@ -1,12 +1,13 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 from tqdm import tqdm
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import ExtraTreesRegressor
+from DQN import DQNAgent
+from FQI import FQIAgent
 import matplotlib.pyplot as plt
 import pickle
 import os
 import numpy as np
+import 
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -18,118 +19,51 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
-    def __init__(self):
-      self.env= TimeLimit(HIVPatient(domain_randomization=False), max_episode_steps=200)
-      self.S = []
-      self.A = []
-      self.R = []
-      self.S2 = []
-      self.D = []
-      self.Qfunctions = []
-      self.Q = None
-      
-    def collect_samples(self, nb_samples, disable_tqdm=False, print_done_states=False):
-      s, _ = self.env.reset()
-      #dataset = []
-      
-      for _ in tqdm(range(nb_samples), disable=disable_tqdm):
-          a = self.env.action_space.sample()
-          s2, r, done, trunc, _ = self.env.step(a)
-          #dataset.append((s,a,r,s2,done,trunc))
-          self.S.append(s)
-          self.A.append(a)
-          self.R.append(r)
-          self.S2.append(s2)
-          self.D.append(done)
-          if done or trunc:
-              s, _ = self.env.reset()
-              if done and print_done_states:
-                  print("done!")
-          else:
-              s = s2
-      self.S = np.array(self.S)
-      self.A = np.array(self.A).reshape((-1,1))
-      self.R = np.array(self.R)
-      self.S2= np.array(self.S2)
-      self.D = np.array(self.D)
-  
+    def __init__():
+        self.agent_type="dqn"
+        if self.agent_type=="dqn":
+            config = {'nb_actions':n_action,
+          'learning_rate': 0.001,
+          'gamma': 0.98,
+          'buffer_size': 20_000,
+          'epsilon_min': 0.01,
+          'epsilon_max': 1.,
+          'epsilon_decay_period': 10_000,
+          'epsilon_delay_decay': 400,
+          'batch_size': 1024,
+          'gradient_steps': 1,
+          'update_target_strategy': 'ema', # or 'replace'
+          'update_target_freq': 600,
+          'update_target_tau': 0.002,
+          'criterion': torch.nn.SmoothL1Loss()}
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            nb_neurons=24
+            state_dim=6
+            n_action=4
+            DQN = torch.nn.Sequential(nn.Linear(state_dim, nb_neurons),
+                          nn.ReLU(),
+                          nn.Linear(nb_neurons, nb_neurons),
+                          nn.ReLU(), 
+                          nn.Linear(nb_neurons, n_action)).to(device)
+            self.agent=DQNAgent(config, DQN)
+        else:
+            print("Specify agent type")
 
-    def rf_fqi(self, iterations, nb_actions, gamma, disable_tqdm=False):
-      nb_samples = self.S.shape[0]
-      
-      self.SA = np.append(self.S,self.A,axis=1)
-      for iter in tqdm(range(iterations), disable=disable_tqdm):
-          if iter==0:
-              value=self.R.copy()
-          else:
-              Q2 = np.zeros((nb_samples,nb_actions))
-              for a2 in range(nb_actions):
-                  A2 = a2*np.ones((self.S.shape[0],1))
-                  S2A2 = np.append(self.S2,A2,axis=1)
-                  Q2[:,a2] =  self.Qfunctions[-1].predict(S2A2)
-              max_Q2 = np.max(Q2,axis=1)
-              value = self.R + gamma*(1-self.D)*max_Q2
-          Q = RandomForestRegressor()
-          Q.fit(self.SA,value)
-          self.Qfunctions.append(Q)
-      self.Q=self.Qfunctions[-1]
-      self.save()
-                    
-    def greedy_action(self, Q,s,nb_actions):
-      Qsa = []
-      for a in range(nb_actions):
-          sa = np.append(s,a).reshape(1, -1)
-          Qsa.append(Q.predict(sa))
-      return np.argmax(Qsa)
-
-    def plot_inital_state(self, nb_iter):
-      s0,_ = self.env.reset()
-      Vs0 = np.zeros(nb_iter)
-      for i in range(nb_iter):
-          Qs0a = []
-          for a in range(self.env.action_space.n):
-              s0a = np.append(s0,a).reshape(1, -1)
-              Qs0a.append(self.Qfunctions[i].predict(s0a))
-          Vs0[i] = np.max(Qs0a)
-      plt.title('Value of inital state across iterations')
-      plt.xlabel("Iterations")
-      plt.ylabel("Value")
-      plt.plot(Vs0)
-
-    def plot_Bellman_residual(self, nb_iter):
-      # Bellman residual
-      residual = []
-      for i in range(1,nb_iter):
-          residual.append(np.mean((self.Qfunctions[i].predict(self.SA)-self.Qfunctions[i-1].predict(self.SA))**2))
-      plt.figure()
-      plt.title("Bellman residual")
-      plt.xlabel("Iterations")
-      plt.ylabel("Residual")
-      plt.plot(residual);
+    def train(self):
+        if self.agent_type=="dqn":
+            max_episode=300
+            agent.train(self, max_episode)
+        else:
+            print("Specify agent type")
 
     def act(self, observation: np.ndarray, use_random: bool = False) -> int:
-        if use_random:
-          action=self.env.action_space.sample()
-        else:
-          action=self.greedy_action(self.Q, observation, 1)
+        action=self.agent.act(observation, use_random)
         return action
 
     def save(self, path: str=None) -> None:
-        # filename = "my_model.pickle"
-        if path is None:
-            path = 'Q_tree.pkl'
-        with open(path, 'wb') as f:
-            pickle.dump(self.Q, f)
-            print("Q saved!")
-
+        self.agent.save(path)
+    
     def load(self) -> None:
-        ### SAVE IN THE REPOSITORY
-        path = 'Q_tree.pkl'
-        if not os.path.exists(path):
-            print("No model to load")
-            return
-        with open(path, 'rb') as f:
-            self.Q = pickle.load(f)
-            print("Q loaded!")
+        self.agent.load()
   
     
